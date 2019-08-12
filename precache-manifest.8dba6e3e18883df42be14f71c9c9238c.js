07007d4896643d55b867661cf92d79ae2ba557ed
Mahmoud Alsati <mahmoud.alsati@gmail.com>
2019-08-12 20:56:39 +0300

    Updates

---
+++ precache-manifest.8dba6e3e18883df42be14f71c9c9238c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cb3c97e9938719a4684d994734e733",
+    "url": "/ens-events-reader/index.html"
+  },
+  {
+    "revision": "e77dd68d04c3e7f2f3a9",
+    "url": "/ens-events-reader/static/css/main.da867661.chunk.css"
+  },
+  {
+    "revision": "cc05adb3217e81bef6c2",
+    "url": "/ens-events-reader/static/js/2.de36e518.chunk.js"
+  },
+  {
+    "revision": "e77dd68d04c3e7f2f3a9",
+    "url": "/ens-events-reader/static/js/main.e0de5ac9.chunk.js"
+  },
+  {
+    "revision": "df04ed0bedd97e997b79",
+    "url": "/ens-events-reader/static/js/runtime~main.acb4dfba.js"
+  }
+]);
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './view/app/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
